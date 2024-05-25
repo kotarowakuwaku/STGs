618fadc24c99a4c6be3dbd82b441491662d210d8
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-05-25 23:59:58 +0900

    画面全体レイアウト調整

--- lib/pages/input/input_ income_page.dart
+++ lib/pages/input/input_ income_page.dart
@@ -13,21 +13,64 @@ class InputIncomePage extends StatelessWidget {
         title: const Text('STGs Simulator'),
       ),
       body: Column(children: [
-        const Text('合計'),
-        const Text('1000円'),
-        const Text('時給'),
-        TextField(
-          decoration: const InputDecoration(
-            labelText: '時給を入力してください',
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 30),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.blue),
           ),
-          controller: incomeController,
+          child: Text(
+            "収入",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
         ),
-        const Text('時間'),
-        TextField(
-          decoration: const InputDecoration(
-            labelText: '時間を入力してください',
+        const Text(
+          '10,000円',
+          style: TextStyle(
+            fontSize: 30,
+          )
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 30),
+          child: Row(
+              children: [
+                SizedBox(width: 50),
+                Text('時給'),
+                SizedBox(width: 50), // 適切な間隔を追加するために使用することもできます
+                Expanded(
+                  child: TextField(
+                    decoration: const InputDecoration(
+                    ),
+                    controller: incomeController,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text('円'),
+                SizedBox(width: 50),
+              ],
+            ),
+        ),
+        Container(
+          margin: EdgeInsets.symmetric(vertical: 30),
+          child: Row(
+            children: [
+              SizedBox(width: 50),
+              Text('時間'),
+              SizedBox(width: 50), // 適切な間隔を追加するために使用することもできます
+              Expanded(
+                child: TextField(
+                  decoration: const InputDecoration(
+                  ),
+                  controller: timeController,
+                ),
+              ),
+              SizedBox(width: 10),
+              Text('h'),
+              SizedBox(width: 50),
+            ],
           ),
-          controller: timeController,
         ),
         ElevatedButton(
             onPressed: () {
