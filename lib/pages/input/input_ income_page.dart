77f4c7f499604a8fcaadbeffd7a78e181eb16f73
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-05-26 14:05:46 +0900

    合計金額計算機能追加

--- lib/pages/input/input_ income_page.dart
+++ lib/pages/input/input_ income_page.dart
@@ -1,49 +1,61 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:save_towards_your_goal_simulator/pages/input/input_spending_page.dart';
 
-class InputIncomePage extends StatelessWidget {
+class InputIncomePage extends StatefulWidget {
   const InputIncomePage({Key? key}) : super(key: key);
 
   @override
+  _InputIncomePageState createState() => _InputIncomePageState();
+}
+
+class _InputIncomePageState extends State<InputIncomePage> {
+  final TextEditingController incomeController = TextEditingController();
+  final TextEditingController timeController = TextEditingController();
+  String income = '0';
+  String time = '0';
+  int result = 0;
+
+  @override
   Widget build(BuildContext context) {
-    final TextEditingController incomeController = TextEditingController();
-    final TextEditingController timeController = TextEditingController();
     return Scaffold(
       appBar: AppBar(
         title: const Text('STGs Simulator'),
       ),
-      body: Column(children: [
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 30),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.blue),
-          ),
-          child: Text(
-            "収入",
-            textAlign: TextAlign.center,
-            style: TextStyle(
-              fontSize: 30,
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 30),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.blue),
+            ),
+            child: Text(
+              "収入",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 30,
+              ),
             ),
           ),
-        ),
-        const Text(
-          '10,000円',
-          style: TextStyle(
-            fontSize: 30,
-          )
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 30),
-          child: Row(
+          (incomeController.text != '' && timeController.text != '')?Text((int.parse(incomeController.text) * int.parse(timeController.text)).toString() + '円'):Text(''),
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 30),
+            child: Row(
               children: [
                 SizedBox(width: 50),
                 Text('時給'),
-                SizedBox(width: 50), // 適切な間隔を追加するために使用することもできます
+                SizedBox(width: 50),
                 Expanded(
                   child: TextField(
-                    decoration: const InputDecoration(
-                    ),
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: const InputDecoration(),
                     controller: incomeController,
+                    onChanged: (value){
+                      setState(() {
+                        income = value;
+                      });
+                    },
                   ),
                 ),
                 SizedBox(width: 10),
@@ -51,37 +63,46 @@ class InputIncomePage extends StatelessWidget {
                 SizedBox(width: 50),
               ],
             ),
-        ),
-        Container(
-          margin: EdgeInsets.symmetric(vertical: 30),
-          child: Row(
-            children: [
-              SizedBox(width: 50),
-              Text('時間'),
-              SizedBox(width: 50), // 適切な間隔を追加するために使用することもできます
-              Expanded(
-                child: TextField(
-                  decoration: const InputDecoration(
-                  ),
-                  controller: timeController,
-                ),
-              ),
-              SizedBox(width: 10),
-              Text('h'),
-              SizedBox(width: 50),
-            ],
           ),
-        ),
-        ElevatedButton(
+          Container(
+            margin: EdgeInsets.symmetric(vertical: 30),
+            child: Row(
+              children: [
+                SizedBox(width: 50),
+                Text('時間'),
+                SizedBox(width: 50),
+                Expanded(
+                  child: TextField(
+                    keyboardType: TextInputType.number,
+                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    decoration: const InputDecoration(),
+                    controller: timeController,
+                    onChanged: (value){
+                      setState(() {
+                        time = value;
+                      });
+                    },
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text('h'),
+                SizedBox(width: 50),
+              ],
+            ),
+          ),
+          ElevatedButton(
             onPressed: () {
               Navigator.push(
                 context,
                 MaterialPageRoute(
-                    builder: (context) => const InputSpendingPage()),
+                  builder: (context) => const InputSpendingPage(),
+                ),
               );
             },
-            child: const Text('支出入力へ')),
-      ]),
+            child: const Text('支出入力へ'),
+          ),
+        ],
+      ),
     );
   }
 }
