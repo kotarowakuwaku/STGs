705a5f818cbacf605487624d18043394341c0615
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-06-20 17:00:10 +0900

    タイトルページ仮完成

---
+++ lib/pages/title_page.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'input/input_ income_page.dart';
+
+class TitlePage extends StatelessWidget {
+  const TitlePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFFFF68B4),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            // アプリ内から画像を表示
+            Image.asset(
+              'images/stgs.jpeg',
+              width: double.infinity,
+            ),
+            const SizedBox(height: 20), // Optional: Add some space between the image and the button
+            TextButton(
+              child: const Text('-スタート-'),
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.yellow,
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => InputIncomePage(),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
