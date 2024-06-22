c3a9b841a15ac631767ed10442e67b36f3b67d1a
kotarowakuwaku <is0668fs@ed.ritsumei.ac.jp>
2024-06-22 23:44:40 +0900

    棒グラフの作成

--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
+
+class _BarChartData {
+  final String category;
+  final int value;
+
+  _BarChartData(this.category, this.value);
+}
 
 class ResultPage extends StatelessWidget {
   final int outcomeData;
@@ -19,202 +27,263 @@ class ResultPage extends StatelessWidget {
         ),
       ),
       body: Padding(
-          padding: const EdgeInsets.all(24.0),
-          child: outcomeData <= incomeData
-              ? Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "結果は・・・",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
-                      ),
+        padding: const EdgeInsets.all(24.0),
+        child: outcomeData <= incomeData
+            ? Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "結果は・・・",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const Row(
-                      children: [
-                        Text(
-                          "プラス",
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.red,
-                          ),
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        "プラス",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.red,
                         ),
-                        Text(
-                          "になります",
-                          style: TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
-                          ),
+                      ),
+                      Text(
+                        "になります",
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      "いいですね！",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
                       ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "いいですね！",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const Text(
-                      "友達とお遊べるときは、",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
+                  ),
+                  const Text(
+                    "友達とお遊びできるときは、",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "遊びましょう！",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "でもお金の使い過ぎはほどほどに",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "差額",
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const Text(
-                      "遊びましょう！",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
+                      const SizedBox(
+                        width: 10,
                       ),
-                    ),
-                    const Text(
-                      "でもお金の使い過ぎはほどほどに",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        "${incomeData - outcomeData}",
+                        style: const TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Color(0xFFFFBF00),
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        "円",
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "差額",
-                          style: TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "${incomeData - outcomeData}",
-                          style: const TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Color(0xFFFFBF00),
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        const Text(
-                          "円",
-                          style: TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
+                      children: <Widget>[
+                        Container(
+                          height: 300,
+                          padding: const EdgeInsets.all(16),
+                          child: _buildBarChart(
+                            outcomeData: outcomeData,
+                            incomeData: incomeData,
                           ),
                         ),
                       ],
                     ),
-                  ],
-                )
-              : Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text(
-                      "結果は・・・",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                ],
+              )
+            : Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    "結果は・・・",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const Row(
-                      children: [
-                        Text(
-                          "マイナス",
-                          style: TextStyle(
-                            fontSize: 30,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.blue,
-                          ),
+                  ),
+                  const Row(
+                    children: [
+                      Text(
+                        "マイナス",
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.blue,
                         ),
-                        Text(
-                          "になります",
-                          style: TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
-                          ),
+                      ),
+                      Text(
+                        "になります",
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
                         ),
-                      ],
-                    ),
-                    const SizedBox(height: 20),
-                    const Text(
-                      "残念ですが、",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
                       ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    "残念ですが、",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
                     ),
-                    const Text(
-                      "おかねが足りませんね",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
+                  ),
+                  const Text(
+                    "おかねが足りませんね",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "支出を少し減らしてみては",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const Text(
+                    "いかがでしょうか？",
+                    style: TextStyle(
+                      fontSize: 21,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        "差額",
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const Text(
-                      "支出を少し減らしてみては",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
+                      const SizedBox(
+                        width: 10,
                       ),
-                    ),
-                    const Text(
-                      "いかがでしょうか？",
-                      style: TextStyle(
-                        fontSize: 21,
-                        fontWeight: FontWeight.bold,
+                      Text(
+                        "${outcomeData - incomeData}",
+                        style: const TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.blue,
+                        ),
                       ),
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
+                      const SizedBox(
+                        width: 10,
+                      ),
+                      const Text(
+                        "円",
+                        style: TextStyle(
+                          fontSize: 21,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Center(
+                    child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "差額",
-                          style: TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "${outcomeData - incomeData}",
-                          style: const TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
-                            decoration: TextDecoration.underline,
-                            decorationColor: Colors.blue,
-                          ),
-                        ),
-                        const SizedBox(
-                          width: 10,
-                        ),
-                        const Text(
-                          "円",
-                          style: TextStyle(
-                            fontSize: 21,
-                            fontWeight: FontWeight.bold,
+                      children: <Widget>[
+                        Container(
+                          height: 300,
+                          padding: const EdgeInsets.all(16),
+                          child: _buildBarChart(
+                            outcomeData: outcomeData,
+                            incomeData: incomeData,
                           ),
                         ),
                       ],
                     ),
-                  ],
-                )),
+                  ),
+                ],
+              ),
+      ),
     );
   }
+}
+
+Widget _buildBarChart({required int outcomeData, required int incomeData}) {
+  final data = [
+    _BarChartData('収入', incomeData),
+    _BarChartData('支出', outcomeData),
+  ];
+
+  var series = [
+    charts.Series<_BarChartData, String>(
+      id: 'Data',
+      domainFn: (datum, _) => datum.category,
+      measureFn: (datum, _) => datum.value,
+      data: data,
+      colorFn: (datum, index) {
+        return datum.category == '収入'
+            ? charts.MaterialPalette.red.shadeDefault
+            : charts.MaterialPalette.green.shadeDefault;
+      },
+      labelAccessorFn: (datum, _) => '',
+    ),
+  ];
+
+  return charts.BarChart(
+    series,
+    animate: true,
+    vertical: true,
+    barRendererDecorator: charts.BarLabelDecorator<String>(),
+    domainAxis: const charts.OrdinalAxisSpec(),
+  );
 }
